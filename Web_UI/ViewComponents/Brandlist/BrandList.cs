2a98f48b80ae1fa807f296c2abd0c5df30996651
EmrahAbay <EmrahAbay@MYPC>
2024-05-10 10:04:27 +0300

    edits

---
+++ Web_UI/ViewComponents/Brandlist/BrandList.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_UI.ViewComponents.Brandlist
+{
+   public class BrandList : ViewComponent
+    {
+        public BrandRepo brandRepo = new();
+        public IViewComponentResult Invoke()
+        {
+            //bu komutumuz ile listeden rastgele 5 veriyi sıraladık.
+            //return View(brandRepo.TList().OrderBy(x => Guid.NewGuid()).Take(5).ToList());
+
+            return View(brandRepo.TList());
+        }
+    }
+}
---
+++ Web_UI/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_UI.ViewComponents.Category
+{
+   public class CategoryList : ViewComponent
+    {
+      public  CategoryRepo categoryRepo = new();
+        public IViewComponentResult Invoke()
+        {
+            return View(categoryRepo.TList());
+        }
+    }
+}
