5e59a1e5a3f23948cc91e3f3230c57762b393f7c
Emrah_Abay <Emrah_Abay@MYPC>
2024-05-13 13:18:59 +0300

    edits

--- Web_UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_UI.Models
-{
-   public class ErrorViewModel
-   {
-      public string? RequestId { get; set; }
-
-      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-   }
-}
--- Web_UI/Program.cs
+++ Web_UI/Program.cs
@@ -6,39 +6,37 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>(x =>
 {
-   x.Password.RequireUppercase = false;
-   x.Password.RequireNonAlphanumeric = false;
+    x.Password.RequireUppercase = false;
+    x.Password.RequireNonAlphanumeric = false;
 })
    .AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
-//bu komutla Proje düzeyinde otorize ettik.
+
 builder.Services.AddMvc(config =>
 {
-   var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-   config.Filters.Add(new AuthorizeFilter(policy));
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
 });
 builder.Services.AddMvc();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(x =>
 {
-   //sistemdeki otantikyeyi 100 dk olarak ayarlar.
-   x.ExpireTimeSpan = TimeSpan.FromMinutes(100);
-   x.AccessDeniedPath = new PathString("/Login/PageDenied/");
-   x.LoginPath = "/Product/Index/";
+    //sistemdeki otantikyeyi 100 dk olarak ayarlar.
+    x.ExpireTimeSpan = TimeSpan.FromMinutes(100);
+    x.AccessDeniedPath = new PathString("/Login/PageDenied/");
+    x.LoginPath = "/Product/Index/";
 });
-
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-   app.UseExceptionHandler("/Home/Error");
-   app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
 app.UseStatusCodePagesWithReExecute("/Login/Page404", "?code={0}");
@@ -48,7 +46,6 @@ app.UseAuthentication();
 app.UseSession();
 app.UseRouting();
 app.UseAuthorization();
-
 
 app.MapControllerRoute(
 name: "areas",
